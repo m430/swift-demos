f062b1ba86e5a756957ba9d821c3b2dd8ad20da2
m430 <360475097@qq.com>
2024-06-05 15:09:34 +0800

    Initial Commit

---
+++ SwiftDemos/SwiftDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemosApp.swift
+//  SwiftDemos
+//
+//  Created by 郑敏 on 2024/6/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
